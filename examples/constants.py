a42e83455c7fc37fbea73373ce60d7b4336dbdae
= <=>
2025-01-18 13:09:02 -0500

    create constants.py and edit notebooks accordingly

---
+++ examples/constants.py
@@ -0,0 +1,17 @@
+# Variable creation of geopackage links for use in the notebooks.abs
+
+
+# DEM Tile Index Geopackage Links
+phase1dem = 'https://kyfromabove.s3-us-west-2.amazonaws.com/elevation/DEM/TileGrids/kyfromabove_phase1_5k_dem_grid.gpkg'
+phase2dem = 'https://kyfromabove.s3-us-west-2.amazonaws.com/elevation/DEM/TileGrids/kyfromabove_phase2_5k_dem_grid.gpkg'
+phase3dem = 'https://kyfromabove.s3-us-west-2.amazonaws.com/elevation/DEM/TileGrids/kyfromabove_phase3_5k_dem_grid.gpkg'
+
+# Point Cloud Tile Index Geopackage Links
+phase1pc = 'https://kyfromabove.s3-us-west-2.amazonaws.com/elevation/PointCloud/TileGrids/kyfromabove_phase1_pointcloud_5k_grid.gpkg'
+phase2pc = 'https://kyfromabove.s3-us-west-2.amazonaws.com/elevation/PointCloud/TileGrids/kyfromabove_phase2_pointcloud_5k_grid.gpkg'
+phase3pc = 'https://kyfromabove.s3-us-west-2.amazonaws.com/elevation/PointCloud/TileGrids/kyfromabove_phase3_pointcloud_5k_grid.gpkg'
+
+# Orthoimagery Tile Index Geopackage Links
+phase1orthos = 'https://kyfromabove.s3-us-west-2.amazonaws.com/imagery/orthos/tile-grids/kyfromabove_phase1_aerial_5k_grid.gpkg'
+phase2orthos = 'https://kyfromabove.s3-us-west-2.amazonaws.com/imagery/orthos/tile-grids/kyfromabove_phase2_aerial_5k_grid.gpkg'
+phase3orthos = 'https://kyfromabove.s3-us-west-2.amazonaws.com/imagery/orthos/tile-grids/kyfromabove_phase3_aerial_5k_grid.gpkg'
